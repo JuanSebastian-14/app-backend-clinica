ec42d4aaa6c8374d2c2d76dd175dbb89b65789c4
JuanEspiritu <sebastiandefray413@outlook.com>
2025-11-13 13:58:08 -0500

    v1

---
+++ src/main/java/flutter_backend/Espiritu/EspirituApplication.java
@@ -0,0 +1,13 @@
+package flutter_backend.Espiritu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspirituApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspirituApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flutter_backend/Espiritu/EspirituApplicationTests.java
@@ -0,0 +1,13 @@
+package flutter_backend.Espiritu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspirituApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
